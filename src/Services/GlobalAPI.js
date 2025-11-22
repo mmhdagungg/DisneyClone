132ac8cacc965a95b4578c6aec00100a3d8ff963
mmhdagungg <mmhdagung7@gmail.com>
2025-11-22 14:47:26 +0700

    fix api

--- src/Services/GlobalAPI.js
+++ src/Services/GlobalAPI.js
@@ -3,9 +3,11 @@ import axios from "axios";
 const movieBaseUrl = "https://api.themoviedb.org/3";
 const apiKey = "1b8afa85c8a315ad8df01aa8bd9e7c05";
 
-const getTrendingVideos = axios.get(this.movieBaseUrl + 
-    "/trending/all/day?api_key"+apiKey);
-
+const getTrendingVideos = () => {
+    return axios.get(
+        movieBaseUrl + "/trending/all/day?api_key="+apiKey
+    );
+}
 export default {
     getTrendingVideos
 }
