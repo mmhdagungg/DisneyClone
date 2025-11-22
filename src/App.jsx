57c56c9ef8557e4eee8ed948235f134f07a00f5f
mmhdagungg <mmhdagung7@gmail.com>
2025-11-22 14:48:50 +0700

    add hide scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header.jsx'
+import Slider from './components/Slider.jsx'
 
 function App() {
 
   return (
     <div className="">
       <Header />
+      <Slider/>
     </div>
   )
 }
