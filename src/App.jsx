bddfc517855df2890a2107201024bf8f1f858c1a
mmhdagungg <mmhdagung7@gmail.com>
2025-11-27 23:06:07 +0700

    add production house

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header.jsx'
 import Slider from './components/Slider.jsx'
+import ProductionHouse from './components/ProductionHouse.jsx'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className="">
       <Header />
       <Slider/>
+      <ProductionHouse/>
     </div>
   )
 }
