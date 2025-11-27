65c51f785293bde9815c938b149ef967dc91448a
mmhdagungg <mmhdagung7@gmail.com>
2025-11-27 23:07:18 +0700

    fix slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import GlobalAPI from '../Services/GlobalAPI'
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+const screenWidth = window.innerWidth;
 export default function Slider(){
     const [movieList, setMovieList] = useState([]);
     const elementRef = useRef();
@@ -18,15 +19,29 @@ export default function Slider(){
         }).catch(err => console.log(err))
     }
 
+    const sliderRight = (element) => {
+        element.scrollLeft += screenWidth - 110
+    }
+
+    const sliderLeft = (element) => {
+        element.scrollLeft -= screenWidth - 110
+    }
     return(
         <div>
-            <HiChevronLeft className='hidden md:block text-white text-[30px] absolute mx-8 mt-[150px]'/>
-            <HiChevronRight className='hidden md:block text-white text-[30px] absolute right-0 mx-8 mt-[150px]'/>
-            <div className='flex overflow-x-auto px-16 py-4 scrollbar-hide' ref={elementRef}>
+            <HiChevronLeft className='hidden md:block text-white text-[30px] 
+                absolute mx-8 mt-[150px] cursor-pointer'
+                onClick={() => sliderLeft(elementRef.current)}/>
+
+            <HiChevronRight className='hidden md:block text-white text-[30px]
+                absolute right-0 mx-8 mt-[150px] cursor-pointer'
+                onClick={() => sliderRight(elementRef.current)}/>
+
+            <div className='flex overflow-x-auto px-16
+                py-4 scrollbar-hide scroll-smooth' ref={elementRef}>
                 {movieList.map((item) => (
                     <img src={IMAGE_BASE_URL + item.backdrop_path}
                     className='min-w-full md:h-[310px] object-cover 
-                    object-left-top mr-5 rounded-md'/>
+                    object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all ease-in duration-200'/>
                 ))}
             </div>
         </div>
