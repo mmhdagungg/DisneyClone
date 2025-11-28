88ca96bdfb4515003157d72e453e7299509a4587
mmhdagungg <mmhdagung7@gmail.com>
2025-11-28 22:54:56 +0700

    add genre

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Header from './components/Header.jsx'
 import Slider from './components/Slider.jsx'
 import ProductionHouse from './components/ProductionHouse.jsx'
-
+import GenreMovieList from './components/GenreMovieList.jsx'
 function App() {
 
   return (
@@ -10,6 +10,7 @@ function App() {
       <Header />
       <Slider/>
       <ProductionHouse/>
+      <GenreMovieList/>
     </div>
   )
 }
