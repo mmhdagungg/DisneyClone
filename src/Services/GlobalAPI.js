ac7e79c8c4b22e6e4e0b23f9b602eca4d09e5701
mmhdagungg <mmhdagung7@gmail.com>
2025-11-28 22:54:30 +0700

    add genre

--- src/Services/GlobalAPI.js
+++ src/Services/GlobalAPI.js
@@ -2,12 +2,18 @@ import axios from "axios";
 
 const movieBaseUrl = "https://api.themoviedb.org/3";
 const apiKey = "1b8afa85c8a315ad8df01aa8bd9e7c05";
-
+const movieByGenreBaseUrl = "https://api.themoviedb.org/3/discover/movie";
 const getTrendingVideos = () => {
     return axios.get(
-        movieBaseUrl + "/trending/all/day?api_key="+apiKey
+        movieBaseUrl + '/trending/all/day?api_key=' + apiKey
     );
 }
+const getMoviesByGenre = (genreId) => {
+    return axios.get(
+         movieByGenreBaseUrl + '?api_key=' + apiKey + '&with_genres=' + genreId
+    )
+}
 export default {
-    getTrendingVideos
+    getTrendingVideos,
+    getMoviesByGenre
 }
