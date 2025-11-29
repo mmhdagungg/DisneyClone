e85b5f0da80233361fe2e88a15daf8e9d0d45fb4
mmhdagungg <mmhdagung7@gmail.com>
2025-11-30 02:24:28 +0700

    add list movie

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,29 @@
 import GlobalAPI from "../Services/GlobalAPI.js";
-import {useEffect} from "react";
-export default function MovieList({genreId}){
-    useEffect(() => {
-        getMovieByGenreId();
-    },[])
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard.jsx";
+export default function MovieList({ genreId }) {
+  useEffect(() => {
+    getMovieByGenreId();
+  }, []);
 
-    const getMovieByGenreId = () => {
-        GlobalAPI.getMovieByGenreId(genreId).then((resp) => {
-            console.log(resp.data.results)    
-        })
-    }
-    return <div>Movie List Component</div>;
+  const [movieList, setMovieList] = useState([]);
+
+  const getMovieByGenreId = () => {
+    GlobalAPI.getMovieByGenreId(genreId).then((resp) => {
+      // console.log(resp.data.results)
+      setMovieList(resp.data.results);
+    });
+  };
+  return (
+    <div>
+      <div
+        className="flex overflow-x-auto gap-8 scrollbar-hide
+                py-5 px-3"
+      >
+        {movieList.map((item, index) => (
+          <MovieCard movie={item} />
+        ))}
+      </div>
+    </div>
+  );
 }
