9e718b39035ec829f3e3e6bff8a39417e7048eb2
mmhdagungg <mmhdagung7@gmail.com>
2025-11-29 23:58:18 +0700

    add Movie List

--- src/components/GenreMovieList.jsx
+++ src/components/GenreMovieList.jsx
@@ -1,11 +1,13 @@
 import GenresList from "../constant/GenresList.jsx";
+import MovieList from "./MovieList.jsx";
 
 export default function GenreMovieList(){
     return <div>
         {GenresList.genere.map((item, index) => index <= 4&&(
             <div className="p-8 px-8 md:px-16" >
                 <h2 className="text-[20px] 
-                font-bold text-white">{item.name}</h2>   
+                font-bold text-white">{item.name}</h2>
+                <MovieList/>   
             </div>    
         ))}
     </div>;
