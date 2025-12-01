c8e32893fe5a3f92df5459f73a0535041afe210d
mmhdagungg <mmhdagung7@gmail.com>
2025-12-01 22:47:33 +0700

    add slider

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,13 @@
 import GlobalAPI from "../Services/GlobalAPI.js";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import MovieCard from "./MovieCard.jsx";
+import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
 export default function MovieList({ genreId }) {
+  const [movieList, setMovieList] = useState([]);
+  const elementRef = useRef();
   useEffect(() => {
     getMovieByGenreId();
   }, []);
-
-  const [movieList, setMovieList] = useState([]);
 
   const getMovieByGenreId = () => {
     GlobalAPI.getMovieByGenreId(genreId).then((resp) => {
@@ -14,16 +15,40 @@ export default function MovieList({ genreId }) {
       setMovieList(resp.data.results);
     });
   };
+  const slideRight = () => {
+    elementRef.current.scrollTo({
+      left: elementRef.current.scrollLeft + 500,
+      behavior: "smooth",
+    });
+  };
+
+  const slideLeft = () => {
+    elementRef.current.scrollTo({
+      left: elementRef.current.scrollLeft - 500,
+      behavior: "smooth",
+    });
+  };
   return (
-    <div>
+    <div className="relative">
+      <IoChevronBackOutline
+        onClick={slideLeft}
+        className="text-[50px] text-white p-2 z-10 cursor-pointer
+        hidden md:block absolute mt-[150px]"
+      />
       <div
         className="flex overflow-x-auto gap-8 scrollbar-hide
                 py-5 px-3"
+        ref={elementRef}
       >
         {movieList.map((item, index) => (
           <MovieCard movie={item} />
         ))}
       </div>
+      <IoChevronForwardOutline
+        onClick={slideRight}
+        className="text-[50px] text-white hidden md:block p-2 z-10 top-0
+         absolute mt-[150px] right-0 cursor-pointer"
+      />
     </div>
   );
 }
