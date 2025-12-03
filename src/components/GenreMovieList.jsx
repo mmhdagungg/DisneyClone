6e7ec03ae73902a0aca7ee84087c1da8852a2c71
mmhdagungg <mmhdagung7@gmail.com>
2025-12-04 00:16:47 +0700

    fix prettier

--- src/components/GenreMovieList.jsx
+++ src/components/GenreMovieList.jsx
@@ -1,14 +1,23 @@
 import GenresList from "../constant/GenresList.jsx";
 import MovieList from "./MovieList.jsx";
 
-export default function GenreMovieList(){
-    return <div>
-        {GenresList.genere.map((item, index) => index <= 4&&(
-            <div className="p-8 px-8 md:px-16" >
-                <h2 className="text-[20px] 
-                font-bold text-white">{item.name}</h2>
-                <MovieList genreId={item.id}/>   
-            </div>    
-        ))}
-    </div>;
+export default function GenreMovieList() {
+  return (
+    <div>
+      {GenresList.genere.map(
+        (item, index) =>
+          index <= 4 && (
+            <div className="p-8 px-8 md:px-16">
+              <h2
+                className="text-[20px] 
+                font-bold text-white"
+              >
+                {item.name}
+              </h2>
+              <MovieList genreId={item.id} index_={index} />
+            </div>
+          )
+      )}
+    </div>
+  );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,13 @@
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
-export default function MovieCard({movie}){
-    return(
-        <>
-            <img src={ IMAGE_BASE_URL + movie.poster_path}
-            className="w-[110px] md:w-[200px] 
+export default function MovieCard({ movie }) {
+  return (
+    <>
+      <img
+        src={IMAGE_BASE_URL + movie.poster_path}
+        className="w-[110px] md:w-[200px] 
             hover:border-[3px] hover:scale-110 border-gray-300 rounded-lg 
-            transition-all duration-150 ease-in"/>
-        </>
-    )
+            transition-all duration-150 ease-in"
+      />
+    </>
+  );
 }
